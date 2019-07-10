31fb2ce6a3c94a1ac84d1b06ef94bd44d2240c30
Антон Антоненко <toxaantonenko@mail.ru>
2019-07-10 04:47:22 +0300

    upd 0.4.1

--- Ivory Role Play Client/MainWindow.xaml.cs
+++ Ivory Role Play Client/MainWindow.xaml.cs
@@ -1,25 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.IO;
+using System;
 
 namespace Ivory_Role_Play_Client
 {
+  
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        string Path = string.Empty;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -32,12 +27,30 @@ namespace Ivory_Role_Play_Client
 
         private void BtnClose_Click(object sender, RoutedEventArgs e)
         {
-            Application.Current.Shutdown();
+            System.Windows.Application.Current.Shutdown();
         }
 
         private void BtnSettings_Click(object sender, RoutedEventArgs e)
         {
-            // потом
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (System.Windows.Forms.OpenFileDialog openFileDialog = new System.Windows.Forms.OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    filePath = openFileDialog.FileName.ToString();
+
+                    using (var profilesKey = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\SAMP",true))
+                    {
+                        profilesKey.SetValue("sampexe", filePath);
+                    }
+
+                }
+            }
         }
 
         private void BtnHide_Click(object sender, RoutedEventArgs e)
@@ -45,9 +58,50 @@ namespace Ivory_Role_Play_Client
             WindowState = WindowState.Minimized;
         }
 
-        private void BtnClose_MouseEnter(object sender, MouseEventArgs e)
+        private void BtnYouTube_Click(object sender, RoutedEventArgs e)
         {
-           
+            System.Diagnostics.Process.Start("https://www.youtube.com/channel/UC-D8z6LkPaiZ2n-pK-ZsAFg");
+        }
+
+        private void BtnVK_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://vk.com/ivoryroleplaye");
+        }
+
+        private void BtnDiscord_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://discord.gg/ggeywTb");
+        }
+
+       
+
+        private void BtnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            if(tbNick.Text != null)
+            {
+                using (var profilesKey = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\SAMP",true))
+                {
+                    profilesKey.SetValue("PlayerName", tbNick.Text);
+                    System.Diagnostics.Process.Start(Path, "176.32.39.69:7777");
+                }
+
+            }
+        }
+
+        private void TbNick_Loaded(object sender, RoutedEventArgs e)
+        {
+            using (var profilesKey = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\SAMP"))
+            {
+                tbNick.Text = profilesKey.GetValue("PlayerName").ToString();
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            using (var profilesKey = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\SAMP"))
+            {
+                Path = profilesKey.GetValue("sampexe").ToString();
+            }
         }
     }
 }
